dcd1ccdb39e867dd610ac86a2170739cd077bb77
Rubinich <alojzrubinic@gmail.com>
2023-06-08 17:16:07 +0200

    audio

--- Assets/Scripts/PhoneController.cs
+++ Assets/Scripts/PhoneController.cs
@@ -1,8 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PhoneController : MonoBehaviour
 {
+    public AudioClip musicClip; // The audio clip to play
+    public AudioSource phoneAudioSource; // The audio source component attached to the phone
 
+    private void OnTriggerEnter(Collider other)
+    {
+            // Check if the phone audio source and music clip are assigned
+            if (phoneAudioSource != null && musicClip != null)
+            {
+                // Play the music clip on the phone audio source
+                phoneAudioSource.clip = musicClip;
+                phoneAudioSource.Play();
+            }
+    }
 }
